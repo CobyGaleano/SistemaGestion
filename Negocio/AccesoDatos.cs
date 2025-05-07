a369a120d3c84a95282357746663b8ad159c6efe
cobygaleano <briang._@hotmail.com>
2025-05-07 00:29:48 -0300

    Actualizacion de servidor SQl

--- Negocio/AccesoDatos.cs
+++ Negocio/AccesoDatos.cs
@@ -30,7 +30,7 @@ namespace Negocio
             //conexion = new SqlConnection("server=.\\SQLEXPRESSLAB3; database=CATALOGO_P3_DB; integrated security=false; user = sa; password = 123456");
             //BRIAN: 
             //conexion = new SqlConnection("server=.\\SQLLABO3; database=CATALOGO_P3_DB; integrated security=false; user = sa; password = 123456");
-            conexion = new SqlConnection("server=.\\SQLLABO3; database=DBSISTEMA_VENTA; integrated security=false; user = sa; password = 123456");
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=DBSISTEMA_VENTA; integrated security=false; user = sa; password = 123456");
             //JOAQUIN:
             //conexion = new SqlConnection("server=.\\SQLEXPRESS01; database=CATALOGO_P3_DB; integrated security=true ");
             comando = new SqlCommand();
