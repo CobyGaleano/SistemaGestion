375347cec2fcb787105cffddde4050a276407053
cobygaleano <briang._@hotmail.com>
2025-06-22 19:59:49 -0300

    AGREGADO SP - AGREGAR, EDITAR Y ELIMINAR (CATEGORIA)

---
+++ Query's - DB/SP - EDITAR, MODIFICIAR Y ELIMINAR (CATEGORIAS).sql
@@ -0,0 +1,64 @@
+--PROCEDIMIENTO CREAR CATEGORIA --
+CREATE PROC SP_RegistrarCategoria(
+@Descripcion VARCHAR(50),
+@Resultado BIT OUTPUT,
+@Mensaje VARCHAR (500) OUTPUT
+)
+AS
+BEGIN
+	SET @Resultado = 0
+	IF NOT EXISTS (SELECT * FROM CATEGORIA WHERE Descripcion = @Descripcion)
+	BEGIN
+		INSERT INTO CATEGORIA(Descripcion) VALUES (@Descripcion)
+		SET @Resultado = SCOPE_IDENTITY()
+		END
+		ELSE
+			SET @Mensaje = 'No se puede repetir la descripcion de una categoria'
+END
+GO
+
+
+--PROCEDIMIENTO MODIFICAR CATEGORIA--
+CREATE PROC SP_EditarCategoria(
+@IdCategoria INT,
+@Descripcion VARCHAR(50),
+@Resultado BIT OUTPUT,
+@Mensaje VARCHAR (500) OUTPUT
+)
+AS
+BEGIN
+	SET @Resultado = 1
+	IF NOT EXISTS (SELECT * FROM CATEGORIA WHERE Descripcion = @Descripcion AND IdCategoria = @IdCategoria)
+	UPDATE CATEGORIA SET
+	Descripcion = @Descripcion
+	WHERE IdCategoria = @IdCategoria
+	ELSE
+	BEGIN
+		SET @Resultado = 0
+		SET @Mensaje = 'No se puede repetir la descripcion de una categoria'
+	END
+END
+GO
+
+--PROCEDIMIENTO ELIMINAR CATEGORIA--
+CREATE PROC SP_EliminarCategoria(
+@IdCategoria INT,
+@Resultado BIT OUTPUT,
+@Mensaje VARCHAR (500) OUTPUT
+)
+AS
+BEGIN
+	SET @Resultado = 1
+	IF NOT EXISTS (
+	SELECT * FROM CATEGORIA C
+	INNER JOIN PRODUCTO P ON P.IdCategoria = C.IdCategoria
+	WHERE C.IdCategoria = @IdCategoria)
+	BEGIN
+		DELETE TOP(1) FROM CATEGORIA WHERE IdCategoria = @IdCategoria
+	END
+	BEGIN
+		SET @Resultado = 0
+		SET @Mensaje = 'La categoria se encuentra relacionada a un producto'
+	END
+END
+GO
