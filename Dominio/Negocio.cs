3c87525cebc7a1027062f320a48f54570d9472d7
cobygaleano <briang._@hotmail.com>
2025-07-16 21:46:54 -0300

    Logica "NegocioNegocio.cs" resta IMAGEN

--- Dominio/Negocio.cs
+++ Dominio/Negocio.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Negocio
+    public class Negocio
     {
+        public int IdNegocio { get; set; }
+        public string Nombre { get; set; }
+        public string RUC { get; set; }
+        public string Direccion { get; set; }
     }
 }
+ 
--- Negocio/NegocioNegocio.cs
+++ Negocio/NegocioNegocio.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using Dominio;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Negocio
 {
-    internal class NegocioNegocio
+    public class NegocioNegocio
     {
+        public Dominio.Negocio ObtenerDatos()
+        {
+            Dominio.Negocio obj = new Dominio.Negocio();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                //datos.setearConsulta("SELECT IdNegocio, Nombre, RUC, Direccion FROM NEGOCIO WHERE IdNegocio = @IdNegocio");
+                //datos.setearParametros("@IdNegocio", obj.IdNegocio);
+                datos.setearConsulta("SELECT IdNegocio, Nombre, RUC, Direccion FROM NEGOCIO WHERE IdNegocio = 1");
+
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    obj = new Dominio.Negocio()
+                    {
+                        IdNegocio = Convert.ToInt32(datos.Lector["IdNegocio"]),
+                        Nombre = datos.Lector["Nombre"].ToString(),
+                        RUC = datos.Lector["RUC"].ToString(),
+                        Direccion = datos.Lector["Direccion"].ToString()
+                    };
+
+                }
+            }
+            catch
+            {
+                obj = new Dominio.Negocio();
+            }
+
+
+            return obj;
+        }
+
+        public bool GuardarDatos(Dominio.Negocio obj, out string mensaje)
+        {
+            mensaje = string.Empty;
+            bool respuesta = true;
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                //datos.setearConsulta("SELECT IdNegocio, Nombre, RUC, Direccion FROM NEGOCIO WHERE IdNegocio = @IdNegocio");
+                //datos.setearParametros("@IdNegocio", obj.IdNegocio);
+                datos.setearConsulta("UPDATE NEGOCIO SET Nombre = @Nombre, RUC = @RUC, Direccion = @Direccion WHERE IdNegocio = 1");
+                datos.setearParametros("@Nombre", obj.Nombre);
+                datos.setearParametros("@RUC", obj.RUC);
+                datos.setearParametros("@Direccion", obj.Direccion);
+
+                if(!datos.ejecutarAccionResultado())
+                {
+                    mensaje = "No se pudo guardar los datos";
+                    respuesta = false;
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                mensaje = ex.Message;
+                respuesta = false;
+            }
+
+
+            return respuesta;
+        }
     }
 }
