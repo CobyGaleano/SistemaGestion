0894d13037d46e19e882958507c89cc70b11d731
cobygaleano <briang._@hotmail.com>
2025-08-09 21:12:11 -0300

    Creacion "CompraNegocio" - (incompleto)

---
+++ Negocio/CompraNegocio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class CompraNegocio
+    {
+        public int ObtenerCorrelativo()
+        {
+            int idCorrelativo = 0;
+
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT COUNT (*) + 1 FROM COMPRA");
+                datos.ejecutarLectura();
+                idCorrelativo = Convert.ToInt32(datos.ejecutarScalar());
+            }
+            catch (Exception ex)
+            {
+                idCorrelativo = 0;
+            }
+            return idCorrelativo;
+        }
+    }
+}
