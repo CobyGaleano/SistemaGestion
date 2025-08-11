0bbe615577dd7a830c9f2371e3b1b4946356411d
cobygaleano <briang._@hotmail.com>
2025-08-10 21:05:12 -0300

    "frmGastos" - funcion (logica) "RegistarCompra"

--- Negocio/CompraNegocio.cs
+++ Negocio/CompraNegocio.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dominio;
+using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,5 +28,36 @@ namespace Negocio
             }
             return idCorrelativo;
         }
+
+        public bool RegistrarCompra(Compra obj, DataTable DetalleCompra, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("sp_RegistrarCompra", true);
+                datos.setearParametros("@IdUsuario", obj.IdCompra);
+                datos.setearParametros("@IdProveedor",obj.oProveedor.IdProveedor);
+                datos.setearParametros("@TipoDocumento",obj.TipoDocumento);
+                datos.setearParametros("@NumeroDocumento",obj.NumeroDocumento);
+                datos.setearParametros("@MontoTotal",obj.MontoTotal);
+                datos.setearParametros("@DetalleCompra", DetalleCompra);
+                datos.setearParametroSalida("@Resultado", SqlDbType.Int);
+                datos.setearParametroSalida("@Mensaje", SqlDbType.VarChar, 500);
+
+                datos.ejecutarAccion();
+                resultado = Convert.ToBoolean(datos.obtenerValorParametro("@Resultado"));
+                Mensaje = datos.obtenerValorParametro("@Mensaje").ToString();
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje += ex.ToString();
+            }
+            return resultado;
+        }
+
+    
     }
 }
