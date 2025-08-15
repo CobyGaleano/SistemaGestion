8ecc6a08a414bd120a2132ba625416651f842806
cobygaleano <briang._@hotmail.com>
2025-08-14 23:01:03 -0300

    Diseño estetica - detalleCompra

---
+++ GestionNegocio/frmDetalleCompra.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace GestionNegocio
+{
+    partial class frmDetalleCompra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContenedor = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.lblNroDocumento = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtTipoDoc = new System.Windows.Forms.TextBox();
+            this.lblTipoDoc = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.gpbInfoCompra = new System.Windows.Forms.GroupBox();
+            this.gpbProveedor = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtNroDoc = new System.Windows.Forms.TextBox();
+            this.dgvDetalleCompra = new System.Windows.Forms.DataGridView();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecioCompra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gpbInfoCompra.SuspendLayout();
+            this.gpbProveedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleCompra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblContenedor
+            // 
+            this.lblContenedor.BackColor = System.Drawing.Color.White;
+            this.lblContenedor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblContenedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContenedor.Location = new System.Drawing.Point(146, 21);
+            this.lblContenedor.Name = "lblContenedor";
+            this.lblContenedor.Size = new System.Drawing.Size(713, 475);
+            this.lblContenedor.TabIndex = 0;
+            this.lblContenedor.Text = "Detalle Compra";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(553, 67);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(100, 20);
+            this.txtDocumento.TabIndex = 1;
+            // 
+            // lblNroDocumento
+            // 
+            this.lblNroDocumento.AutoSize = true;
+            this.lblNroDocumento.BackColor = System.Drawing.Color.White;
+            this.lblNroDocumento.Location = new System.Drawing.Point(429, 70);
+            this.lblNroDocumento.Name = "lblNroDocumento";
+            this.lblNroDocumento.Size = new System.Drawing.Size(118, 13);
+            this.lblNroDocumento.TabIndex = 2;
+            this.lblNroDocumento.Text = "Numero de documento:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(740, 64);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(659, 64);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.BackColor = System.Drawing.Color.White;
+            this.lblFecha.Location = new System.Drawing.Point(6, 23);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 6;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(9, 39);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(164, 20);
+            this.txtFecha.TabIndex = 5;
+            // 
+            // txtTipoDoc
+            // 
+            this.txtTipoDoc.Location = new System.Drawing.Point(197, 39);
+            this.txtTipoDoc.Name = "txtTipoDoc";
+            this.txtTipoDoc.Size = new System.Drawing.Size(209, 20);
+            this.txtTipoDoc.TabIndex = 3;
+            // 
+            // lblTipoDoc
+            // 
+            this.lblTipoDoc.AutoSize = true;
+            this.lblTipoDoc.BackColor = System.Drawing.Color.White;
+            this.lblTipoDoc.Location = new System.Drawing.Point(194, 23);
+            this.lblTipoDoc.Name = "lblTipoDoc";
+            this.lblTipoDoc.Size = new System.Drawing.Size(89, 13);
+            this.lblTipoDoc.TabIndex = 4;
+            this.lblTipoDoc.Text = "Tipo Documento:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(426, 39);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(192, 20);
+            this.txtUsuario.TabIndex = 7;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.White;
+            this.lblUsuario.Location = new System.Drawing.Point(423, 23);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 8;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // gpbInfoCompra
+            // 
+            this.gpbInfoCompra.BackColor = System.Drawing.Color.White;
+            this.gpbInfoCompra.Controls.Add(this.lblUsuario);
+            this.gpbInfoCompra.Controls.Add(this.txtUsuario);
+            this.gpbInfoCompra.Controls.Add(this.lblFecha);
+            this.gpbInfoCompra.Controls.Add(this.txtFecha);
+            this.gpbInfoCompra.Controls.Add(this.lblTipoDoc);
+            this.gpbInfoCompra.Controls.Add(this.txtTipoDoc);
+            this.gpbInfoCompra.Location = new System.Drawing.Point(179, 93);
+            this.gpbInfoCompra.Name = "gpbInfoCompra";
+            this.gpbInfoCompra.Size = new System.Drawing.Size(636, 77);
+            this.gpbInfoCompra.TabIndex = 4;
+            this.gpbInfoCompra.TabStop = false;
+            this.gpbInfoCompra.Text = "Información compra:";
+            // 
+            // gpbProveedor
+            // 
+            this.gpbProveedor.BackColor = System.Drawing.Color.White;
+            this.gpbProveedor.Controls.Add(this.txtNroDoc);
+            this.gpbProveedor.Controls.Add(this.label3);
+            this.gpbProveedor.Controls.Add(this.textBox2);
+            this.gpbProveedor.Controls.Add(this.label4);
+            this.gpbProveedor.Controls.Add(this.textBox3);
+            this.gpbProveedor.Location = new System.Drawing.Point(179, 176);
+            this.gpbProveedor.Name = "gpbProveedor";
+            this.gpbProveedor.Size = new System.Drawing.Size(636, 77);
+            this.gpbProveedor.TabIndex = 5;
+            this.gpbProveedor.TabStop = false;
+            this.gpbProveedor.Text = "Información proveedor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(6, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Numero Documento:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(194, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Razon Social:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(209, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // txtNroDoc
+            // 
+            this.txtNroDoc.Location = new System.Drawing.Point(561, 39);
+            this.txtNroDoc.Name = "txtNroDoc";
+            this.txtNroDoc.Size = new System.Drawing.Size(57, 20);
+            this.txtNroDoc.TabIndex = 8;
+            // 
+            // dgvDetalleCompra
+            // 
+            this.dgvDetalleCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalleCompra.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Producto,
+            this.PrecioCompra,
+            this.Cantidad,
+            this.SubTotal});
+            this.dgvDetalleCompra.Location = new System.Drawing.Point(179, 259);
+            this.dgvDetalleCompra.Name = "dgvDetalleCompra";
+            this.dgvDetalleCompra.Size = new System.Drawing.Size(636, 223);
+            this.dgvDetalleCompra.TabIndex = 6;
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            this.Producto.Width = 200;
+            // 
+            // PrecioCompra
+            // 
+            this.PrecioCompra.HeaderText = "PrecioCompra";
+            this.PrecioCompra.Name = "PrecioCompra";
+            this.PrecioCompra.Width = 150;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // SubTotal
+            // 
+            this.SubTotal.HeaderText = "SubTotal";
+            this.SubTotal.Name = "SubTotal";
+            this.SubTotal.Width = 150;
+            // 
+            // frmDetalleCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(50)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(1055, 535);
+            this.Controls.Add(this.dgvDetalleCompra);
+            this.Controls.Add(this.gpbProveedor);
+            this.Controls.Add(this.gpbInfoCompra);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lblNroDocumento);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.lblContenedor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmDetalleCompra";
+            this.Text = "frmDetalleCompra";
+            this.gpbInfoCompra.ResumeLayout(false);
+            this.gpbInfoCompra.PerformLayout();
+            this.gpbProveedor.ResumeLayout(false);
+            this.gpbProveedor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleCompra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblContenedor;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Label lblNroDocumento;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtTipoDoc;
+        private System.Windows.Forms.Label lblTipoDoc;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.GroupBox gpbInfoCompra;
+        private System.Windows.Forms.GroupBox gpbProveedor;
+        private System.Windows.Forms.TextBox txtNroDoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dgvDetalleCompra;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecioCompra;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubTotal;
+    }
+}
